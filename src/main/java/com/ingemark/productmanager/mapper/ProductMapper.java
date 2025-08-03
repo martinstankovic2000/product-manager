fec59e7412d457625abab4d3867fdb8ca8b95f74
martinstankovic2000 <martinstankovic2000@gmail.com>
2025-08-03 19:40:38 +0200

    Removed mapstruct from project, updated message locale, updated mapper and added builder method to Product entity

--- src/main/java/com/ingemark/productmanager/mapper/ProductMapper.java
+++ src/main/java/com/ingemark/productmanager/mapper/ProductMapper.java
@@ -4,25 +4,34 @@ import com.ingemark.productmanager.model.CreateProductDto;
 import com.ingemark.productmanager.model.PagedProductResponseDto;
 import com.ingemark.productmanager.model.Product;
 import com.ingemark.productmanager.model.ProductResponseDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
-public interface ProductMapper {
+@Component
+public class ProductMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "code", ignore = true)
-    @Mapping(target = "priceUsd", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    Product toEntity(CreateProductDto createDto);
+    public Product toEntity(CreateProductDto createDto) {
+        return Product.builder()
+                .name(createDto.name())
+                .priceEur(createDto.priceEur())
+                .isAvailable(createDto.isAvailable())
+                .build();
+    }
 
-    ProductResponseDto toResponseDto(Product product);
+    public ProductResponseDto toResponseDto(Product product) {
+        return new ProductResponseDto(
+                product.getName(),
+                product.getCode(),
+                product.getPriceEur(),
+                product.getPriceUsd(),
+                product.getIsAvailable()
+        );
 
-    default PagedProductResponseDto toPagedProductResponseDto(Page<Product> products) {
+    }
+
+    public PagedProductResponseDto toPagedProductResponseDto(Page<Product> products) {
         List<ProductResponseDto> content = products.getContent()
                 .stream()
                 .map(this::toResponseDto)
