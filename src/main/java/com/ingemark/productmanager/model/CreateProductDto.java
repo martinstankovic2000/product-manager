29d3d35cc530a700d194f2c9a205b22438165d67
martinstankovic2000 <martinstankovic2000@gmail.com>
2025-08-03 18:49:50 +0200

    Updated validation messages

--- src/main/java/com/ingemark/productmanager/model/CreateProductDto.java
+++ src/main/java/com/ingemark/productmanager/model/CreateProductDto.java
@@ -7,13 +7,13 @@ import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public record CreateProductDto(
-        @NotBlank(message = "Name is required")
+        @NotBlank(message = "{product.name.blank}")
         String name,
 
-        @NotNull(message = "Price EUR is required")
-        @DecimalMin(value = "0.0", message = "Price EUR must be >= 0")
+        @NotNull(message = "{product.price.required}")
+        @DecimalMin(value = "0.0", message = "{product.price.negative}")
         BigDecimal priceEur,
 
-        @NotNull(message = "Availability status is required")
+        @NotNull(message = "{product.availability.required}")
         Boolean isAvailable
 ){}
